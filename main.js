5a90caf4259633b4bc50c849f755f0ba150b0add
baglana <102351104+baglana@users.noreply.github.com>
2024-01-03 14:23:33 +0600

    Add operator functions

---
+++ main.js
@@ -0,0 +1,19 @@
+// add
+function getSum(num1, num2) {
+  return num1 + num2;
+}
+
+// subtract
+function getDifference(num1, num2) {
+  return num1 - num2;
+}
+
+// multiply
+function getProduct(num1, num2) {
+  return num1 * num2;
+}
+
+// divide
+function getQuotient(num1, num2) {
+  return num1 / num2;
+}
