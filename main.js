65297b6137c09ab83e4035f5ea40ab302287099c
baglana <102351104+baglana@users.noreply.github.com>
2024-01-03 14:31:17 +0600

    Add operate function

--- main.js
+++ main.js
@@ -17,3 +17,24 @@ function getProduct(num1, num2) {
 function getQuotient(num1, num2) {
   return num1 / num2;
 }
+
+function operate(operator, num1, num2) {
+  switch (operator) {
+    case '+':
+      return getSum(num1, num2);
+    case '-':
+      return getDifference(num1, num2);
+    case '*':
+      return getProduct(num1, num2);
+    case '/':
+      return getQuotient(num1, num2);
+    default:
+      console.error('Unknown operator');
+  }
+}
+
+let num1 = 1;
+let num2 = -0.5;
+let operator = '/';
+
+console.log(operate(operator, num1, num2));
